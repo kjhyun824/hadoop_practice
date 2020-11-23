4269695c7f9dd44f080919e050b1526b0d2043d1
kjhyun824 <woori4829@gmail.com>
2020-11-23 14:05:02 +0000

    [FIXME] assertThat & is method not exist

--- test/Test.java
+++ test/Test.java
@@ -1,21 +1,36 @@
 import org.apache.hadoop.conf.Configuration;
 
-Configuration conf = new Configuration();
-conf.addResource("configuration1.xml");
-assertThat(conf.get("color"), is("yellow"));
-assertThat(conf.getInt("size", 0), is(10));
-assertThat(conf.get("breadth", "wide"), is("wide"));
+public class Test {
+	public static void main(String[] args) {
+		Configuration conf = new Configuration();
+		conf.addResource("configuration1.xml");
+		/*
+		assertThat(conf.get("color"), is("yellow"));
+		assertThat(conf.getInt("size", 0), is(10));
+		assertThat(conf.get("breadth", "wide"), is("wide"));
+		*/
+		assertThat(conf.get("color"), "yellow");
+		assertThat(conf.getInt("size", 0), 10);
+		assertThat(conf.get("breadth", "wide"), "wide");
 
-Configuration conf2 = new Configuration();
-conf2.addResource("configuration1.xml");
-conf2.addResource("configuration2.xml");
 
-assertThat(conf2.getInt("size", 0), is(12));
-assertThat(conf2.get("weight"), is("heavy"));
+		Configuration conf2 = new Configuration();
+		conf2.addResource("configuration1.xml");
+		conf2.addResource("configuration2.xml");
 
-assertThat(conf2.get("size-weight"), is("12, heavy"));
-System.setProperty("size", 14);
-assertThat(conf2.get("size-weight"), is("14, heavy"));
+		/*
+		assertThat(conf2.getInt("size", 0), is(12));
+		assertThat(conf2.get("weight"), is("heavy"));
+		*/
+		assertThat(conf2.getInt("size", 0), 12);
+		assertThat(conf2.get("weight"), "heavy");
 
-System.setProperty("length", "2");
-assertThat(conf2.get("length"), is((String) null));
+		assertThat(conf2.get("size-weight"), ("12, heavy"));
+		System.setProperty("size", "14");
+		assertThat(conf2.get("size-weight"), ("14, heavy"));
+
+		System.setProperty("length", "2");
+		assertThat(conf2.get("length"), (String) null);
+	}
+}
+
---
+++ test/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export PATH=${PATH}:${JAVA_HOME}
+
+javac Test.java -cp $(hadoop classpath)
